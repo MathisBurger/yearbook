6ec8ed19b2bce7461175ea5dace6db41d274a4e0
Mathis Burger <handymathis@gmail.com>
2024-04-18 13:34:55 +0200

    [TASK] Added first login page

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+/**
+ * UserFixtures
+ */
+class UserFixtures extends Fixture
+{
+    private readonly UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        $admin = (new User())
+            ->setUsername('admin');
+        $admin->setPassword($this->userPasswordHasher->hashPassword($admin, 'admin123'));
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
