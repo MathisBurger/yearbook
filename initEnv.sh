e65d5ad4462cac39009e61f5a931370bd77e8c2c
Mathis Burger <handymathis@gmail.com>
2024-04-19 18:14:24 +0200

    [TASK] Updated component names

--- initEnv.sh
+++ initEnv.sh
@@ -2,4 +2,7 @@ rm .env
 echo "DATABASE_URL=$DATABASE_URL" >> .env
 echo "APP_ENV=prod" >> .env
 echo "APP_SECRET=81a1c2b0472a6ab9a62a56d74d1c8eca" >> .env
+if [ -d "/var/www/html/var" ]; then
+  mkdir "/var/www/html/var"
+fi
 chown -R www-data:www-data /var/www/html/var
