8d0bef86d755b616d6538652284fec3c9abe7bcf
Mathis Burger <handymathis@gmail.com>
2024-04-19 18:44:39 +0200

    [BUGFIX] Fixed some bugs

--- initEnv.sh
+++ initEnv.sh
@@ -2,3 +2,4 @@ rm .env
 echo "DATABASE_URL=$DATABASE_URL" >> .env
 echo "APP_ENV=prod" >> .env
 echo "APP_SECRET=81a1c2b0472a6ab9a62a56d74d1c8eca" >> .env
+chown -R www-data:www-data /var/www/html/var
